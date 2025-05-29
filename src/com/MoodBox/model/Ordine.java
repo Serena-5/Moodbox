1d3c1996d56f8f69747b3af07cbc78fd64c50977
Serena-5 <s.russo144@studenti.unisa.it>
2025-05-29 17:51:58 +0200

    Creato DAO ordine e modificato ordine.java

--- src/com/MoodBox/model/Ordine.java
+++ src/com/MoodBox/model/Ordine.java
@@ -6,41 +6,39 @@ import java.time.LocalDateTime;
 public class Ordine {
     private int id;
     private int utenteId;
-    private int spedizioneId;
-    private int statoId;
+    private String indirizzoSpedizione;
+    private String metodoSpedizione;
+    private BigDecimal costoSpedizione;
+    private String statoOrdine;
     private LocalDateTime dataOrdine;
     private BigDecimal totale;
     private String noteOrdine;
-    
-    // Costruttori
-    public Ordine() {}
-    
-    public Ordine(int utenteId, int spedizioneId, int statoId, BigDecimal totale) {
-        this.utenteId = utenteId;
-        this.spedizioneId = spedizioneId;
-        this.statoId = statoId;
-        this.totale = totale;
-    }
-    
-    // Getter e Setter
+
+    // Getters e Setters
     public int getId() { return id; }
     public void setId(int id) { this.id = id; }
-    
+
     public int getUtenteId() { return utenteId; }
     public void setUtenteId(int utenteId) { this.utenteId = utenteId; }
-    
-    public int getSpedizioneId() { return spedizioneId; }
-    public void setSpedizioneId(int spedizioneId) { this.spedizioneId = spedizioneId; }
-    
-    public int getStatoId() { return statoId; }
-    public void setStatoId(int statoId) { this.statoId = statoId; }
-    
+
+    public String getIndirizzoSpedizione() { return indirizzoSpedizione; }
+    public void setIndirizzoSpedizione(String indirizzoSpedizione) { this.indirizzoSpedizione = indirizzoSpedizione; }
+
+    public String getMetodoSpedizione() { return metodoSpedizione; }
+    public void setMetodoSpedizione(String metodoSpedizione) { this.metodoSpedizione = metodoSpedizione; }
+
+    public BigDecimal getCostoSpedizione() { return costoSpedizione; }
+    public void setCostoSpedizione(BigDecimal costoSpedizione) { this.costoSpedizione = costoSpedizione; }
+
+    public String getStatoOrdine() { return statoOrdine; }
+    public void setStatoOrdine(String statoOrdine) { this.statoOrdine = statoOrdine; }
+
     public LocalDateTime getDataOrdine() { return dataOrdine; }
     public void setDataOrdine(LocalDateTime dataOrdine) { this.dataOrdine = dataOrdine; }
-    
+
     public BigDecimal getTotale() { return totale; }
     public void setTotale(BigDecimal totale) { this.totale = totale; }
-    
+
     public String getNoteOrdine() { return noteOrdine; }
     public void setNoteOrdine(String noteOrdine) { this.noteOrdine = noteOrdine; }
 }
